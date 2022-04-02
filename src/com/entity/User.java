45bf464125a4bd0fc6d5703e4e5e4e3756ee014c
Fang <857559565@qq.com>
2022-04-02 16:19:23 +0800

    实现头像上传功能

--- src/com/entity/User.java
+++ src/com/entity/User.java
@@ -11,6 +11,33 @@ public class User {
     private String city;
     private String birthday;
     private Integer id;
+    private String img1;
+    private String img2;
+    private String img3;
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
 
     public Integer getId() {
         return id;
